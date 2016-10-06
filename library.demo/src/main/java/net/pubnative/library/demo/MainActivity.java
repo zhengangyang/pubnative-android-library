cf375544f366d6ae4a1e0bcf75d2378cf6cf809c
David Martin <david@martinmacias.com>
2016-10-06 12:26:17 +0200

    v1.4.5

--- library.demo/src/main/java/net/pubnative/library/demo/MainActivity.java
+++ library.demo/src/main/java/net/pubnative/library/demo/MainActivity.java
@@ -3,8 +3,6 @@ package net.pubnative.library.demo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
@@ -59,28 +57,6 @@ public class MainActivity extends ActionBarActivity implements OnClickListener, 
     {
         super.onDestroy();
         Pubnative.onDestroy();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
